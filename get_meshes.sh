d14e7b09b0dc1058bf557f7d38dfc1563c19419a
Luca Pegolotti <luca.pegolotti@epfl.ch>
2019-09-06 13:29:15 +0200

    Update link in get_meshes.sh

--- get_meshes.sh
+++ get_meshes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://www.dropbox.com/s/ljvfl11swa9g1as/meshes.zip?dl=0 > meshes.zip
+curl -L https://www.dropbox.com/s/x6aqbub3qjqd527/meshes.zip?dl=0 > meshes.zip
 unzip meshes.zip
 rm meshes.zip
 rm -r -f __MACOSX/
