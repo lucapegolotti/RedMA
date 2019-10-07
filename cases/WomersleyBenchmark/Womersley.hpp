d9f5a4e2cb0d5f45841e689062dac4bc963689cf
Luca Pegolotti <luca.pegolotti@epfl.ch>
2019-10-07 23:45:26 +0200

    Add womersley example

---
+++ cases/WomersleyBenchmark/Womersley.hpp
@@ -0,0 +1,96 @@
+// Reduced Modeling of Arteries (RedMA)
+// Copyright (C) 2019  Luca Pegolotti
+//
+// RedMA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RedMA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef WOMERSLEY_HPP
+#define WOMERSLEY_HPP 1
+
+#include <complex>
+
+#include <lifev/core/LifeV.hpp>
+#include <lifev/core/filter/GetPot.hpp>
+
+namespace LifeV
+{
+
+class Womersley
+{
+public:
+    static double f(const double& t, const double& x, const double& y,
+                  const double& z, const ID& i);
+
+    static double xexact(const double& t, const double& x, const double& y,
+                       const double& z, const ID& i);
+    static double uexact(const double& t, const double& x, const double& y,
+                       const double& z, const ID& i);
+    static double uexact_dt(const double& t, const double& x, const double& y,
+                          const double& z, const ID& i);
+    static double uexact_dtdt(const double& t, const double& x, const double& y,
+                            const double& z, const ID& i);
+    static double pexact (const double& t, const double& x, const double& y,
+                        const double& z, const ID& i);
+
+    static double pexact_dt(const double& t, const double& x, const double& y,
+                          const double& z, const ID& i);
+
+    // Initial velocity
+    static double x0(const double& t, const double& x, const double& y,
+                   const double& z, const ID& i);
+
+    static double u0(const double& t, const double& x, const double& y,
+                   const double& z, const ID& i);
+
+    static double p0(const double& t, const double& x, const double& y,
+                   const double& z, const ID& i);
+
+    static double grad_u(const UInt& icoor, const double& t, const double& x, const double& y,
+                       const double& z, const ID& i);
+
+    static double fNeumann(const double& t, const double& x, const double& y,
+                         const double& z, const ID& i);
+
+    static double normalVector(const double& t, const double& x, const double& y,
+                             const double& z, const ID& i);
+
+    static double fShearStress(const double& t, const double& x, const double& y,
+                             const double& z, const ID& i);
+
+    static double fWallShearStress(const double& t, const double& x, const double& y,
+                                 const double& z, const ID& i);
+    static void setParamsFromGetPot(const GetPot& dataFile);
+    static void computeQuantities();
+    static void setPeriod(const double& newPeriod);
+    static double getPeriod();
+    static void showMe();
+
+private:
+
+    static Int  S_flagStrain;
+    static double S_mu;
+    static double S_nu;
+    static double S_D;
+    static double S_T;
+    static double S_rho;
+    static double S_W0;
+    static double S_L;
+    static double S_A;
+    static double S_w;
+    static std::complex<double> S_cj1, S_cy0, S_cy1, S_cj0p, S_cj1p, S_cy0p;
+    static std::complex<double> S_cy1p , S_z1, S_b1 , S_ii, S_wi;
+}; // class Womersley
+
+} // namespace LifeV
+
+#endif /* WOMERSLEY_HPP */
---
+++ cases/WomersleyBenchmark/WomersleyBenchmark.cpp
@@ -0,0 +1,67 @@
+// Reduced Modeling of Arteries (RedMA)
+// Copyright (C) 2019  Luca Pegolotti
+//
+// RedMA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RedMA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <Epetra_ConfigDefs.h>
+#ifdef EPETRA_MPI
+#include <mpi.h>
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+
+#include <functional>
+
+#include <GlobalSolver.hpp>
+#include <NavierStokesAssembler.hpp>
+#include <PseudoFSIAssembler.hpp>
+
+#include <lifev/core/filter/GetPot.hpp>
+
+#include "ud_functions.hpp"
+
+using namespace RedMA;
+
+#define COEFF 10
+
+double maxLaw(double t)
+{
+    return LifeV::pressureWomerTime(t,0,0,0,0);
+}
+
+double maxLawDt(double t)
+{
+    return LifeV::pressuredtWomerTime(t,0,0,0,0);
+}
+
+int main(int argc, char **argv)
+{
+    #ifdef HAVE_MPI
+    MPI_Init (nullptr, nullptr);
+    std::shared_ptr<Epetra_Comm> comm (new Epetra_MpiComm(MPI_COMM_WORLD));
+    #else
+    std::shared_ptr<Epetra_Comm> comm(new Epetra_SerialComm());
+    #endif
+
+    GetPot datafile("data");
+    bool verbose = comm->MyPID() == 0;
+    GlobalSolver<NavierStokesAssembler> gs(datafile, comm, verbose);
+    gs.setExportNorms("norms_nonconforming.txt");
+    gs.setLawInflow(std::function<double(double)>(maxLaw));
+    gs.setLawDtInflow(std::function<double(double)>(maxLawDt));
+    gs.solve();
+
+    return 0;
+}
