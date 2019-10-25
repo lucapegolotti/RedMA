1f15bb8567f05932327a87513535ce6d91e3a835
Luca Pegolotti <luca.pegolotti@epfl.ch>
2019-10-25 11:02:06 +0200

    Add SegmentationParser and example

---
+++ examples/ConvertSegmentation/ConvertSegmentation.cpp
@@ -0,0 +1,40 @@
+// Reduced Modeling of Arteries (RedMA)
+// Copyright (C) 2019  Luca Pegolotti
+//
+// RedMA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RedMA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <Epetra_ConfigDefs.h>
+#ifdef EPETRA_MPI
+#include <mpi.h>
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+
+#include <SegmentationParser.hpp>
+
+using namespace RedMA;
+
+int main(int argc, char **argv)
+{
+    #ifdef HAVE_MPI
+    MPI_Init (nullptr, nullptr);
+    std::shared_ptr<Epetra_Comm> comm (new Epetra_MpiComm(MPI_COMM_WORLD));
+    #else
+    std::shared_ptr<Epetra_Comm> comm(new Epetra_SerialComm ());
+    #endif
+
+
+    return 0;
+}
---
+++ geometric_structure/SegmentationParser.cpp
@@ -0,0 +1,25 @@
+#include "SegmentationParser.hpp"
+
+namespace RedMA
+{
+
+SegmentationParser::
+SegmentationParser()
+{
+
+}
+
+SegmentationParser::
+~SegmentationParser()
+{
+
+}
+
+TreeStructure
+SegmentationParser::
+parse(std::string pthName, std::string ctgrName)
+{
+    
+}
+
+}  // namespace RedMA
