14f09e97e92d2166c2f507fe8cdabc9f4fe98ad6
Luca <luca.pegolotti@epfl.ch>
2021-06-04 09:08:05 +0200

    Fix undefined behaviour of bneumann bcs

--- redma/boundary_conditions/BCManager.cpp
+++ redma/boundary_conditions/BCManager.cpp
@@ -220,6 +220,7 @@ getNeumannBc(const double& time, const double& flag, const double& rate)
     //     return 0.0;
     //
     // return -M_models[flag]->getNeumannCondition(time, rate);
+    return 0.0;
 }
 
 double
@@ -231,6 +232,7 @@ getNeumannJacobian(const double& time, const double& flag, const double& rate)
     //     return 0.0;
     //
     // return -M_models[flag]->getNeumannJacobian(time, rate);
+    return 0.0;
 }
 
 void
