fdeb75fb019b04f41070cf86b5c503f47de1a964
federicobetti99 <74817555+federicobetti99@users.noreply.github.com>
2022-04-08 15:21:12 +0200

    Added class for snapshots of Steady Navier Stokes

---
+++ redma/reduced_basis/SnapshotsSteadySampler.cpp
@@ -0,0 +1,218 @@
+//
+// Created by betti on 4/6/22.
+//
+
+#include "SnapshotsSteadySampler.h"
+
+namespace RedMA
+{
+    SnapshotsSteadySampler::
+    SnapshotsSteadySampler(const DataContainer &data, EPETRACOMM comm, unsigned int numSamples) :
+            M_data(data),
+            M_comm(comm), M_sampler(numSamples) {
+    }
+
+
+    void
+    SnapshotsSteadySampler::
+    takeSnapshots(const unsigned int &Nstart) {
+
+        std::vector <std::map<std::string, double>> SnapshotsSamples =
+                M_sampler.getSamples(M_numSamples, M_sampler.M_generatingVector,
+                                     M_sampler.M_paramsNames, M_sampler.M_paramsBounds);
+
+        std::string outdir = M_data("rb/offline/snapshots/directory", "snapshots");
+        int numInletConditions = M_data("bc_conditions/numinletbcs", 1);
+        unsigned int numOutletConditions = M_data("bc_conditions/numoutletbcs", 0);
+
+        fs::create_directory(outdir);
+        GeometryPrinter printer;
+
+        std::vector<double> array_params;
+
+        double elapsedTime = 0.0;
+        double elapsedTimeNoSetup = 0.0;
+
+        for (unsigned int i = 0; i < M_numSamples; i++) {
+
+            std::map<std::string, double> vec = SnapshotsSamples[i];
+
+            std::vector<double> array_params;
+
+            for (auto it = vec.begin(); it != vec.end(); ++ it)
+            {
+                array_params.push_back(it->second);
+            }
+
+            // to guarantee (almost...) that two snapshots are not saved at the same location!
+            unsigned int paramIndex = Nstart;
+
+            // we find the first parameter index available, starting from Nstart
+            while (fs::exists(outdir + "/param" + std::to_string(paramIndex)))
+                paramIndex++;
+            std::string curdir = outdir + "/param" + std::to_string(paramIndex);
+
+            // setting the flow rate at the inlets
+            std::vector<double> vec_inflow = {vec["flow_rate"], 1.0 - vec["flow_rate"]};
+
+            auto inletBC = std::bind(M_inflow,
+                                     std::placeholders::_1,
+                                     vec_inflow[0], vec_inflow[1]);
+
+            for (unsigned int numInlet = 0; numInlet < numInletConditions; numInlet++) {
+                std::function<double(double)> inletDirichlet = [vec_inflow, numInlet, inletBC] (double t)
+                        { return (vec_inflow[numInlet]) * inletBC(t); };
+                M_data.setInletBC(inletDirichlet, numInlet);
+            }
+
+            unsigned int numOutlet = 0;
+            std::function < double(double) > outletDirichlet = [vec_inflow, numOutlet, inletBC] (double t)
+                        { return (vec_inflow[0] + vec_inflow[1]) * inletBC(t); };
+            M_data.setOutletBC(outletDirichlet, numOutlet);
+
+            GlobalProblem problem(M_data, M_comm, false);
+
+            problem.doStoreSolutions();
+
+            fs::create_directory(curdir);
+
+            problem.getTree().setGeometricParametersFromSample(vec);
+
+            problem.setup();
+
+            if (!problem.isFEProblem())
+                throw new Exception("The tree must be composed of only FE nodes to "
+                                    "sample the snapshots!");
+
+            std::string filename = curdir + "/tree.xml";
+            printer.saveToFile(problem.getTree(), filename, M_comm);
+
+            problem.solveSteady();
+
+            dumpSnapshots(problem, curdir, array_params);
+        }
+
+        std::string msg = "Average time per snapshot =  ";
+        msg += std::to_string(elapsedTime);
+        msg += " seconds\n";
+        printlog(MAGENTA, msg, true);
+
+        msg = "Average time per snapshot (no setup) =  ";
+        msg += std::to_string(elapsedTimeNoSetup);
+        msg += " seconds\n";
+        printlog(MAGENTA, msg, true);
+
+    }
+
+    void
+    SnapshotsSteadySampler::
+    dumpSnapshots(GlobalProblem &problem,
+                  std::string outdir,
+                  const std::vector<double> array_params = {}) {
+        auto IDmeshTypeMap = problem.getBlockAssembler()->getIDMeshTypeMap();
+        auto solutions = problem.getSolutions();
+
+        assert(!(solutions.empty()));
+
+        unsigned int n_primal_blocks = IDmeshTypeMap.size();
+        unsigned int n_dual_blocks = solutions[0]->nRows() - n_primal_blocks;
+
+        auto M_mass = problem.getBlockAssembler()->block(0)->assembleMatrix(0);
+        auto M_stiffness = problem.getBlockAssembler()->block(0)->assembleMatrix(1);
+        auto M_divergence = problem.getBlockAssembler()->block(0)->assembleMatrix(2);
+
+        unsigned int takeEvery = M_data("rb/offline/snapshots/take_every", 5);
+        bool binary = M_data("rb/offline/snapshots/dumpbinary", true);
+        bool computereynolds = M_data("rb/offline/snapshots/computereynolds", true);
+        double density = M_data("rb/offline/fluid/density", 1.0);
+        double viscosity = M_data("rb/offline/fluid/viscosity", 1.0);
+
+        std::ios_base::openmode omode = std::ios_base::app;
+        if (binary)
+            omode = omode | std::ios::binary;
+
+        M_mass->block(0, 0)->dump("M");
+        M_stiffness->block(0, 0)->dump("A");
+        M_divergence->block(0, 1)->dump("BdivT");
+        M_divergence->block(1, 0)->dump("Bdiv");
+
+        /*for (auto sol : solutions)
+            problem.getBlockAssembler()->applyPiola(sol, true);*/
+
+        for (auto idmeshtype: IDmeshTypeMap) {
+            std::string meshtypedir = outdir + "/" + idmeshtype.second;
+            fs::create_directory(meshtypedir);
+
+            unsigned int nfields = solutions[0]->block(idmeshtype.first)->nRows();
+
+            for (unsigned int i = 0; i < nfields; i++) {
+                std::string outfilename = meshtypedir + "/field" + std::to_string(i) + ".snap";
+
+                std::ofstream outfile;
+                outfile.open(outfilename, omode);
+                unsigned int count = 0;
+                for (auto sol: solutions) {
+                    auto solBlck = convert<DistributedVector>(convert<BlockVector>(
+                            convert<BlockVector>(sol)->block(idmeshtype.first))->block(i));
+                    if (count % takeEvery == 0) {
+                        std::string str2write = solBlck->getString(',') + "\n";
+                        if (M_comm->MyPID() == 0)
+                            outfile.write(str2write.c_str(), str2write.size());
+                    }
+
+                    count++;
+                }
+                outfile.close();
+            }
+
+
+            for (unsigned int j = 0; j < n_dual_blocks; j++)  // save Lagrange multipliers, if any
+
+            {
+                std::string outfilename = meshtypedir + "/lagmult_" + std::to_string(j) + ".snap";
+                std::ofstream outfile;
+                outfile.open(outfilename, omode);
+                unsigned int count = 0;
+                unsigned int cur_index = n_primal_blocks + j;
+                for (auto sol: solutions) {
+                    auto solBlck = convert<DistributedVector>(convert<BlockVector>(
+                            convert<BlockVector>(sol)->block(cur_index))->block(0));
+                    if (count % takeEvery == 0) {
+                        std::string str2write = solBlck->getString(',') + "\n";
+                        if (M_comm->MyPID() == 0)
+                            outfile.write(str2write.c_str(), str2write.size());
+                    }
+                    count++;
+                }
+                outfile.close();
+            }
+
+            if (computereynolds) {
+                std::ofstream reynoldsfile;
+                reynoldsfile.open(outdir + "/reynolds.txt", std::ios_base::app |
+                                                            std::ios::binary);
+                for (auto sol: solutions) {
+                    auto solBlck = convert<DistributedVector>(convert<BlockVector>(
+                            convert<BlockVector>(sol)->block(idmeshtype.first))->block(0));
+                    double Umax = solBlck->maxMagnitude3D();
+                    auto tNode = problem.getBlockAssembler()->block(0)->getTreeNode();
+                    double D = 2 * tNode->M_block->getInlet(0).M_radius;
+                    double curReynolds = Umax * density * D / viscosity;
+
+                    if (M_comm->MyPID() == 0)
+                        reynoldsfile << std::to_string(curReynolds) << std::endl;
+                }
+                reynoldsfile.close();
+            }
+
+            if (!array_params.empty()) {
+                std::ofstream file(outdir + "/coeffile.txt", std::ios_base::app);
+
+                for (double i: array_params)
+                    file << std::fixed << std::setprecision(10) << i << std::endl;
+
+                file.close();
+            }
+        }
+    }
+}
---
+++ redma/reduced_basis/SnapshotsSteadySampler.h
@@ -0,0 +1,57 @@
+// Reduced Modeling of Arteries (RedMA)
+// Copyright (C) 2019  Luca Pegolotti
+//
+// RedMA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RedMA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SNAPSHOTSSTEADYSAMPLER_HPP
+#define SNAPSHOTSSTEADYSAMPLER_HPP
+
+#include <redma/RedMA.hpp>
+#include <redma/problem/DataContainer.hpp>
+#include <redma/problem/GlobalProblem.hpp>
+#include <redma/reduced_basis/QMC_sampling.h>
+#include <redma/geometry/GeometryPrinter.hpp>
+
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <random>
+
+namespace RedMA
+{
+
+/// Class handling the generation of the steady snapshots.
+    class SnapshotsSteadySampler
+    {
+    public:
+        SnapshotsSteadySampler(const DataContainer& data, EPETRACOMM comm, unsigned int numSamples);
+
+        /// Take the snapshots.
+        void takeSnapshots(const unsigned int& Nstart = 0);
+
+        inline void setInflow(const std::function<double(double,double,double)>& inflow) {M_inflow=inflow;};
+
+        void dumpSnapshots(GlobalProblem& problem, std::string outdir, const std::vector<double> array_params);
+
+    private:
+        DataContainer                                       M_data;
+        EPETRACOMM                                          M_comm;
+        QMC_sampling                                        M_sampler;
+        unsigned int                                        M_numSamples;
+        std::function<double(double,double,double)>         M_inflow;
+    };
+
+}  // namespace RedMA
+
+#endif  // SNAPSHOTSSTEADYSAMPLER_HPP
